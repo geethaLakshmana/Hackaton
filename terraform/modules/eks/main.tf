b81aba8178491fe727ed9c3e22928428e0b68fb6
geethaLakshmana <lgeetharao@gmail.com>
2025-07-19 16:03:04 +0530

    adding main file

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -35,15 +35,6 @@ module "eks_cluster" {
   }
 }
 
-# ... (rest of the file remains the same)
-data "aws_eks_cluster" "cluster" {
-  name = module.eks_cluster.cluster_name
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks_cluster.cluster_name
-}
-
 resource "kubernetes_config_map" "aws_auth" {
   metadata {
     name = "aws-auth"
